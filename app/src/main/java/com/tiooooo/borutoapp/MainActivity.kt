a997c823d8130ce5603b07cb800babbbb79894e5
Tio <tiofani03@gmail.com>
2023-04-23 18:58:00 +0700

    initial navigation

--- app/src/main/java/com/tiooooo/borutoapp/MainActivity.kt
+++ app/src/main/java/com/tiooooo/borutoapp/MainActivity.kt
@@ -10,14 +10,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.tiooooo.borutoapp.navigation.SetupNavGraph
 import com.tiooooo.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/tiooooo/borutoapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.tiooooo.borutoapp.utils
+
+object Constants {
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
